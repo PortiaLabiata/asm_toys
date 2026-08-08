125b114095abde050ba4d45c6fa5f508acbc674b
Ilya Filippov <themorgot@yandex.ru>
2026-08-08 15:51:18 +0300

    Improved help code

--- database.asm
+++ database.asm
@@ -21,25 +21,15 @@ _loop:
     strcmp command_buffer, exit_name
     je exit_func
 
-    strcmp command_buffer, noop_name
-    je noop_func
-
     jmp _loop
 
 help_func:
-    println "This is a simple database. Available commands:"
-    println "help - prints this message"
-    println "exit - exits the program"
-    println "noop - no operation"
+    print help_string, help_string_len 
     jmp _loop
 
 exit_func:
     println "Exiting"
     exit 0
-
-noop_func:
-    println "NOP"
-    jmp _loop
 
 section ".data" writable
     command_buffer rb command_bufsize
@@ -49,6 +39,10 @@ section ".rodata"
     prompt_size = $ - prompt
     command_bufsize = 128
 
+    help_string db "This is a simple database. Available commands:",0xa, \
+        "help - prints this message",0xa, \
+        "exit - exits the program",0xa
+    help_string_len = $ - help_string
+
     help_name db "help",0x0
     exit_name db "exit",0x0
-    noop_name db "noop",0x0
